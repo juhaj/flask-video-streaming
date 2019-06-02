19a8c308f0951c197e0b63ae139a4ae593917be2
Juha Jaykka <juhaj@iki.fi>
2019-06-02 10:38:20 +0100

    Changed Content-Type and mimetype.
    This introduces compatibility with kivy Video class, but breaks compatibility with web browsers.

--- app.py
+++ app.py
@@ -25,15 +25,14 @@ def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='image/jpeg')
 
 
 if __name__ == '__main__':
